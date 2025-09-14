690ac5dc02fc3f66ac7018677f9a581b5e43b365
Vitthal Garule <vitthalgarule113@gmail.com>
2025-09-14 18:00:46 +0530

    update

--- rest/models.py
+++ rest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
--- rest/views.py
+++ rest/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializer
 
-# Create your views here.
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
